fa94effbeca4cc4f30fd0311b258ad9ae308cd37
MustafaAmer-1 <53836898+MustafaAmer-1@users.noreply.github.com>
2021-06-01 21:59:24 +0200

    Calculate Total Distance Function
    Add Calculate Total Distance Function to main file and updates the header file.
    
    Co-Authored-By: Muhammad Ahmad Thabet <53876326+ZeusDes@users.noreply.github.com>

--- Header.h
+++ Header.h
@@ -3,6 +3,9 @@
 #include "Math.h"
 
 #define magicKey 0x4C4F434B
+#define PI 3.14159265358979323846
 
 // Functions Prototypes
 void PortB_Init();
+double distance(double lat1, double lon1, double lat2, double lon2);
+double deg2rad(double deg);
