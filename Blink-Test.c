6579b1ef94b6b2127af3b148a35207825e86e244
MustafaAmer-1 <53836898+MustafaAmer-1@users.noreply.github.com>
2021-06-01 21:10:14 +0200

    Initialize the project
    Initialize the project with starter files to test the working environment.

---
+++ Blink-Test.c
@@ -0,0 +1,11 @@
+#include "Header.h"
+
+int main( void ){
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    GPIO_PORTF_DEN_R = 0x0E;
+    GPIO_PORTF_DIR_R = 0x0E;
+
+    while(1){
+        GPIO_PORTF_DATA_R = 0x04;
+    }
+}
---
+++ Header.h
@@ -0,0 +1,4 @@
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+#include "Math.h"
+#define magicKey 0x4C4F434B
