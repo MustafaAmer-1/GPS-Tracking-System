d272366aebd73ae80836e814e01b1837fa318d0e
mizo7757 <moazayman@hotmail.com>
2021-06-02 19:51:00 +0200

    LED control function
    Controlling the LED ( lighting )
    
    Co-Authored-By: abdurrahman10 <69390033+abdurrahman10@users.noreply.github.com>

--- Header.h
+++ Header.h
@@ -4,6 +4,10 @@
 
 #define magicKey 0x4C4F434B
 #define PI 3.14159265358979323846
+#define OFF 0x00
+#define RED 0x02
+#define BLUE 0x04
+#define GREEN 0x08
 
 // Functions Prototypes
 void PortB_Init();
@@ -11,3 +15,5 @@ void PortF_Init(void);
 void PortD_Init();
 double distance(double lat1, double lon1, double lat2, double lon2);
 double deg2rad(double deg);
+void led_control(int control);
+void check_destination();
